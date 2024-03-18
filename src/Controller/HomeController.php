6101146134074d0ca7b5ff25efc56b7e3a44999c
Mardonis Alves Bezerra <mardonis.bezerra@gmail.com>
2024-03-18 18:06:07 -0300

    mvc layout

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,8 @@ namespace Cyber\Controller;
 use Cyber\Controller\BaseController;
 use Cyber\HttpRequest\HttpRequest;
 use Slim\Views\Twig;
+use Cyber\Header\Header;
+
 
  class  HomeController extends BaseController{
 public function  __construct($request, $response, Twig $view) {
@@ -12,15 +14,36 @@ public function  __construct($request, $response, Twig $view) {
 
 public function home(){
     try {
-        
-            $data = HttpRequest::convertCoins($this->request, $this->response);
             return $this->view->render($this->response, 'index.twig', [
-                'data' => $data
+                'data' => []
             ]);
-       
-        
-    } catch (\Throwable $th) {
-      
+    } catch (\Throwable $e) {
+        return Header::validateRequest($this->response, '500', $e->getMessage());
     }
 }
+
+public function convert(){
+    try {
+        $formData = $this->request->getParsedBody();
+
+        if (isset($formData['moeda'])) {
+            // Acessa o valor da moeda
+            $moeda = $formData['moeda'];
+            var_dump($formData['moeda']);
+            // Restante do seu código...
+        } else {
+            // Campo 'moeda' não encontrado, lida com isso adequadamente
+        }
+      
+
+     
+        $data = HttpRequest::convertCoins($this->response, $formData);
+        return $this->view->render($this->response, 'index.twig', [
+            'data' => $data
+        ]);
+    } catch (\Throwable $e) {
+      
+        return Header::validateRequest($this->response, '500', $e->getMessage());
+    }
+    }
 }
--- src/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$apiKey = 'Adicone a chave da API aqui';
-$baseCurrency = 'BRL';
